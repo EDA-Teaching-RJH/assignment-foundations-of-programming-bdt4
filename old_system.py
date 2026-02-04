6f2b7c26486470d5c7ac02353b344632f7795501
bdt4 <bdt4@kent.ac.uk>
2026-02-04 05:06:30 +0000

    Swapped the range to actually just measure the list rather than hard limit it, stupid on my part

--- old_system.py
+++ old_system.py
@@ -29,7 +29,7 @@ def run_system_monolith():
         if opt == "1":  
             print("Current Crew List:")
             
-            for i in range(4):
+            for i in range(len(n)):
                 print(n[i] + " - " + r[i]) 
                 
         elif opt == "2":
