8eb31b4ec74c45581cb43c9e1fcb5db17f2174ed
bdt4 <bdt4@kent.ac.uk>
2026-02-15 21:02:54 +0000

    Added display_menu()

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,12 +1,49 @@
-def init_database(): #My database function that houses all of the lists that I need
+#My database function that houses all of the lists that I need
+def init_database(): 
     names = ["Picard", "Riker", "Data", "Worf", "Crusher"]
     ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Commander"]
-    divs = ["Command", "Command", "Operations", "Security", "Sciences"]
+    divs = ["Command", "Command", "Operations", "Operations", "Sciences"]
     ids = ["1", "2", "3", "4", "5"]
-    return names, ranks, divs, ids #allows main to use all 4 of my lists
+    #allows main to use all 4 of my lists in paralell
+    return names, ranks, divs, ids 
+
+def display_menu():
+    #ask user name for future use, as well as removing empty spaces and capitlising it
+    user_name = input("Please enter your full name").strip().title()
+    #List of menu options to use later
+    valid_options = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    while True:
+        print("<<< FLEET MANAGER MENU >>>")
+        print("Currently logged in as:", user_name) #Ask for users name, then display it for them
+
+        print("1. Display current crew")
+        print("2. Add member")
+        print("3. Remove member")
+        print("4. Update rank")
+        print("5. Search crew")
+        print("6. Filter by division")
+        print("7. Calculate payroll")
+        print("8. Count officers")
+        print("9. Exit")
+        #Displays the list of options and then asks to select an option
+        #This is all in a while loop so that if the user enters anything except the desired input
+        #We will loop infinitely until they do
+        option = input("Select an option (1-9):").strip()
+
+        if option in valid_options:
+            return option
+        #Checks if the user selects the correct input, otherwise the else statement triggers and we loop
+        else:
+            print("\n INVALID OPTION CHOSEN. \n") #The spacers just make it pretty in my opinion
+
 def main(): #function for the main program that utilises my 10 base functions
     names, ranks, divs, ids = init_database() #calling the variable and stores the lists
     print("Database initialised.") #mostly a diagnostic tool really, but looks cool
+    
     for i in range(len(names)): #just literally testing if my lists work, will remove later
         print(names[i], ranks[i], divs[i], ids[i])
+    
+    option = display_menu()
+    print("You chose:", option) #testing purposes
 main()
